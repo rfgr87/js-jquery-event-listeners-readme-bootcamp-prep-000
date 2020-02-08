fbcb1b5b78df09949510700bca6d1b7e099c99fe
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-08 18:01:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,62 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  alert("Hey!")
+}
 
-// call functions here
+$('p').on("click", getIt());
 
-});
+
+
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+
+$(document).on('keydown', pressIt(key));
+
+function submitIt(){
+  alert('Your form is going to be submitted now.')
+}
+
+$("form").on("submit", submitIt());
+
+
+
+/*
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
+*/
