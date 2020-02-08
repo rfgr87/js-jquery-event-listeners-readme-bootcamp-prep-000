eaa1ef8e1d8a31a9e875f11c65e1ef06ffd93ca1
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-08 18:01:07 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -30,3 +30,33 @@ function submitIt(){
 }
 
 $("form").on("submit", submitIt());
+
+
+
+/*
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
+*/
